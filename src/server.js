8512e55642a5c8ead913039ebe437cb77488630c
AndriyV2504 <andreyvolyanik@gmail.com>
2024-09-14 14:18:20 +0300

    middlware reexport

--- src/server.js
+++ src/server.js
@@ -1,13 +1,26 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
+import { env } from './utils/env.js';
+import { ENV_VARS } from './constants/index.js';
+import {
+  notFoundMiddleware,
+  errorHandlerMiddleware,
+} from './middlewares/index.js';
+
+const PORT = env(ENV_VARS.PORT, 3000);
 
 export const setupServer = () => {
   const app = express();
-  app.use(pino);
   app.use(cors());
 
-  const PORT = 3000;
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
 
   app.use(express.json());
 
@@ -22,12 +35,9 @@ export const setupServer = () => {
     });
   });
 
-  app.use('*', (req, res, next) => {
-    res.status(404).json({
-      message: 'Not found',
-    });
-    next();
-  });
+  app.use(notFoundMiddleware);
+
+  app.use(errorHandlerMiddleware);
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
