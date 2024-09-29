10acb6cdca44b346cb85c40273b0caedaee23bf1
AndriyV2504 <andreyvolyanik@gmail.com>
2024-09-29 19:20:54 +0300

    error corr

--- src/middlewares/isValidId.js
+++ src/middlewares/isValidId.js
@@ -4,7 +4,7 @@ import { isValidObjectId } from 'mongoose';
 export const isValidId =
   (idName = 'contactId') =>
   (req, res, next) => {
-    const { contactId } = req.params[idName];
+    const contactId = req.params[idName];
 
     if (!isValidObjectId(contactId)) {
       return next(createHttpError(400, 'Invalid contact ID'));
